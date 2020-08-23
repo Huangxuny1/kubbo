855833a3a8ecb5285d7b1339120ce73cceef4e35
Huangxuny1 <ghuangxunyi@gmail.com>
2020-08-23 12:10:44 +0800

    brench

---
+++ src/test/java/brenchmark/TestQPSJUnit.java
@@ -0,0 +1,37 @@
+package brenchmark;
+
+import com.huangxunyi.proxy.cglib.CGLIBProxyFactory;
+import com.huangxunyi.remoting.server.KubboServer;
+import com.huangxunyi.rpc.KubboInvoker;
+import demo.IFoo;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestQPSJUnit {
+
+    private static KubboServer server;
+    @BeforeAll
+    public static void setUp(){
+        server = new KubboServer();
+        server.start();
+    }
+
+    @Test
+    public void test(){
+        IFoo proxy = new CGLIBProxyFactory().getProxy(IFoo.class, new KubboInvoker<>(IFoo.class));
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            System.out.println(i + " return " + proxy.say("world"));
+        }
+        long end = System.currentTimeMillis() - start;
+        System.out.println("cost " + end);
+        System.out.println("QPS :  "+ (100000/(end/1000f)) +" /s");
+    }
+
+
+    @AfterAll
+    public static void tearDown(){
+        server.shutdown();
+    }
+}
